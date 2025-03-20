c77c1772df0fe9ad19bc5560e71b20b8da8dafcd
aayushjohari <aayushjohari0403@gmail.com>
2025-03-20 20:04:44 +0530

    initialcommit

---
+++ PYTHON/basics.py
@@ -0,0 +1,38 @@
+print("hello world")
+
+#string
+a="rahul"
+#indexing
+print(a[2])
+
+#slicing
+print(a[-4:-1])
+
+#string methods
+str="Aayush"
+print(str.upper())
+print(str.lower())
+print(str.split())
+print(str.strip())
+print(str.find('y'))
+str2= "Johari"
+print(str + " " +str2)
+
+#tuples
+tuple= (1, 2.3 , "python" , True)
+print(tuple[1:3])
+
+#OPERATORS
+print(tuple*2)
+
+print(tuple.count(1))
+
+#list
+ls=[1,2,3,4,5]
+print(ls[1:3])
+ls.append(6)
+print(ls)
+ls.insert(3,7)
+print(ls)
+ls.sort()
+print(ls)
